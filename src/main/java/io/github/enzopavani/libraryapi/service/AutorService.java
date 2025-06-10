c1b9c033ed29ce77d804558a9f47d4c9655114a4
Enzo <tijoloenso@gmail.com>
2025-06-10 17:47:53 -0300

    Search by example

--- src/main/java/io/github/enzopavani/libraryapi/service/AutorService.java
+++ src/main/java/io/github/enzopavani/libraryapi/service/AutorService.java
@@ -6,6 +6,8 @@ import io.github.enzopavani.libraryapi.repository.AutorRepository;
 import io.github.enzopavani.libraryapi.repository.LivroRepository;
 import io.github.enzopavani.libraryapi.validator.AutorValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -48,6 +50,21 @@ public class AutorService {
             return repository.findByNacionalidade(nacionalidade);
         }
         return repository.findAll();
+    }
+
+    public List<Autor> pesquisaByExample(String nome, String nacionalidade) {
+        var autor = new Autor();
+        autor.setNome(nome);
+        autor.setNacionalidade(nacionalidade);
+
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnorePaths("id", "dataNascimento", "dataCadastro")
+                .withIgnoreNullValues()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example<Autor> autorExample = Example.of(autor, matcher);
+        return repository.findAll(autorExample);
     }
 
     public void atualizar(Autor autor) {
