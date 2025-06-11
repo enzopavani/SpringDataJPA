a8a0ac0c23a139966cc9f32c176df1a880f87950
Enzo <tijoloenso@gmail.com>
2025-06-11 16:46:57 -0300

    POST Livro

--- src/main/java/io/github/enzopavani/libraryapi/controller/LivroController.java
+++ src/main/java/io/github/enzopavani/libraryapi/controller/LivroController.java
@@ -2,7 +2,9 @@ package io.github.enzopavani.libraryapi.controller;
 
 import io.github.enzopavani.libraryapi.controller.dto.CadastroLivroDTO;
 import io.github.enzopavani.libraryapi.controller.dto.ErroResposta;
+import io.github.enzopavani.libraryapi.controller.mappers.LivroMapper;
 import io.github.enzopavani.libraryapi.exceptions.RegistroDuplicadoException;
+import io.github.enzopavani.libraryapi.model.Livro;
 import io.github.enzopavani.libraryapi.service.LivroService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class LivroController {
 
     private final LivroService service;
+    private final LivroMapper mapper;
 
     @PostMapping
     public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO dto) {
         try {
-
-            return ResponseEntity.ok(dto);
+            Livro livro = mapper.toEntity(dto);
+            service.salvar(livro);
+            return ResponseEntity.ok(livro);
         } catch(RegistroDuplicadoException e) {
             var erroDTO = ErroResposta.conflito(e.getMessage());
             return ResponseEntity.status(erroDTO.status()).body(erroDTO);
---
+++ src/main/java/io/github/enzopavani/libraryapi/controller/mappers/LivroMapper.java
@@ -0,0 +1,18 @@
+package io.github.enzopavani.libraryapi.controller.mappers;
+
+import io.github.enzopavani.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.enzopavani.libraryapi.model.Livro;
+import io.github.enzopavani.libraryapi.repository.AutorRepository;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(componentModel = "spring")
+public abstract class LivroMapper {
+
+    @Autowired
+    AutorRepository autorRepository;
+
+    @Mapping(target = "autor", expression = "java( autorRepository.findById(dto.idAutor()).orElse(null) )")
+    public abstract Livro toEntity(CadastroLivroDTO dto);
+}
